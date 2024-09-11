7e1e6c5f85a7e525e461e8d45388305cd9a98c32
Ana beatriz <beatrizanabeatriz743@gmail.com>
2024-09-11 12:51:21 +0000

    primeira e bissexto

---
+++ segundo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    char car;                                       //declara var.  
+    printf("Digite um caractere e ENTER\n");        // mostra na tela
+    scanf("%d",&car);                              // %c para
+    printf ("Caractere digitado e' %d\n",car);
+    return 0;
+    // mostrou na tela valor de car lido
+    // fim main
+
+
+
+
+
+}
